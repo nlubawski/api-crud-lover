601d6d18d9621200ec3ce632d30cbb4b893cc056
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-24 12:16:48 -0300

    feat: create deleteCar

--- src/main/java/com/crud/apicrudlover/Controller/CarController.java
+++ src/main/java/com/crud/apicrudlover/Controller/CarController.java
@@ -3,7 +3,9 @@ package com.crud.apicrudlover.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,10 @@ public class CarController {
   @PostMapping
   public void create(@RequestBody CarDTO req) {
     repository.save(new Car(req));
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteCar(@PathVariable Long id){
+    repository.deleteById(id);
   }
 }
